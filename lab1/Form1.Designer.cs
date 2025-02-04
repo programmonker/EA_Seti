f910af9c7966474c9e9c01b001ffe40880fe875b
Пользователь <@DESKTOP-OSA7UQE>
2025-02-04 10:21:17 +0400

    Добавьте файлы проекта.

---
+++ lab1/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace lab1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            button2 = new Button();
+            label1 = new Label();
+            label2 = new Label();
+            inputBox = new RichTextBox();
+            outputBox = new RichTextBox();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(320, 43);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 0;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(320, 95);
+            button2.Name = "button2";
+            button2.Size = new Size(75, 23);
+            button2.TabIndex = 1;
+            button2.Text = "button2";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(12, 3);
+            label1.Name = "label1";
+            label1.Size = new Size(38, 15);
+            label1.TabIndex = 2;
+            label1.Text = "label1";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(401, 3);
+            label2.Name = "label2";
+            label2.Size = new Size(38, 15);
+            label2.TabIndex = 3;
+            label2.Text = "label2";
+            // 
+            // inputBox
+            // 
+            inputBox.Location = new Point(12, 21);
+            inputBox.Name = "inputBox";
+            inputBox.Size = new Size(302, 190);
+            inputBox.TabIndex = 5;
+            inputBox.Text = "";
+            // 
+            // outputBox
+            // 
+            outputBox.Location = new Point(401, 21);
+            outputBox.Name = "outputBox";
+            outputBox.ReadOnly = true;
+            outputBox.Size = new Size(302, 190);
+            outputBox.TabIndex = 6;
+            outputBox.Text = "";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(734, 450);
+            Controls.Add(outputBox);
+            Controls.Add(inputBox);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button1;
+        private Button button2;
+        private Label label1;
+        private Label label2;
+        private RichTextBox inputBox;
+        private RichTextBox outputBox;
+    }
+}
